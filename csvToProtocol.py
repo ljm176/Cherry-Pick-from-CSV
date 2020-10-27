fd728824fa997639ddd47a2eecedb444f304518a
ljm176 <43517811+ljm176@users.noreply.github.com>
2020-10-27 16:09:17 +0100

    Optional protocol name and add labware to template

--- csvToProtocol.py
+++ csvToProtocol.py
@@ -9,9 +9,16 @@ import csv
 
 csv_file = sys.argv[1]
 
+try:
+    protocol_name = sys.argv[2]
+except:
+    protocol_name = "NewProt.py"
+    
+
 def get_nested_list(c):
     with open(c, "r") as f:
         csvread = csv.reader(f, delimiter = ";")
+        next(csvread)
         t = [row for row in csvread]
     return (t)
 
@@ -19,9 +26,9 @@ x = get_nested_list(csv_file)
 
 prot = open("protocol.txt", "r")
 
-new_prot = open("new_prot.py", "w")
+new_prot = open(protocol_name, "w")
 new_prot.write("t_list = " + str(x))
 for line in prot:
     new_prot.write(line)
-
+    
 new_prot.close()
